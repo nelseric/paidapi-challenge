8f2946f7acbaf07c05815b6f59d13a5ff4806bc2
Eric Nelson <nelseric.alt@gmail.com>
2015-08-31 12:59:25 -0400

    Added start of shunting yard algorithm, and tests

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,6 +1,28 @@
+require 'pry'
 class Calculator
-  def initialize(args)
-    
+  def self.result expression
+    new(expression).result
+  end
+
+  def initialize expression
+    @expression = expression
+  end
+
+  def result
+    expr = postfix_respression
+
+    binding.pry
+  end
+
+  def postfix_respression
+    output = []
+    op_stack = []
+    @expression.scan(/([.\d]+)|([\+\-\*()])/) do |number, op|
+      output << number.to_f if number
+      if op
+        op_stack.push op
+      end
+    end
   end
    
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -6,4 +6,40 @@ describe Calculator do
   it "runs a test" do
     expect(true). to eq false
   end
+
+  context "Addition" do
+    skip
+    it "adds two numbers" dos
+    pass
+      expect(Calculator.result("1+1")).to eql 2
+    end
+    it "adds many numbers" do
+      pass
+      expect(Calculator.result("1+2+3+4+1+2+3+4")).to eql 20
+    end
+  end
+
+  context "Subtraction" do
+    it "subtracts two numbers" do
+      expect(Calculator.result("1+1")).to eql 2
+    end
+    it "adds and subtracts numbers" do
+      expect(Calculator.result("1+2-3+4")).to eql 20
+    end
+  end
+
+  context "Multiplication" do
+    it "multiplies two numbers" do
+      expect(Calculator.result("5*5")).to eql 25
+    end
+  end
+
+  context "Division" do
+    it "divides two numbers evenly" do
+      expect(Calculator.result("10/5")).to eql 2.0
+    end
+    it "divides two numbers with a decimal result" do
+      expect(Calculator.result("15/5")).to eql 2.5
+    end
+  end
 end
