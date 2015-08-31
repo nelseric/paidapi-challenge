be5b1d36a49a7e270e7600a75afac2c5faf1cfb1
Eric Nelson <nelseric.alt@gmail.com>
2015-08-31 13:14:22 -0400

    Single expression operations

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -11,18 +11,53 @@ class Calculator
   def result
     expr = postfix_respression
 
-    binding.pry
+    # binding.pry
+
+    stack = []
+    expr.each do |token|
+      if token.is_a? Numeric
+        stack.push token
+      else
+        case token
+        when "+"
+          b = stack.pop
+          a = stack.pop
+          stack.push a + b
+        when "-"
+          b = stack.pop
+          a = stack.pop
+          stack.push a - b
+        when "*"
+          b = stack.pop
+          a = stack.pop
+          stack.push a * b
+        when "/"
+          b = stack.pop
+          a = stack.pop
+          stack.push a / b
+        else
+          rails "#{token} was not caught"
+        end
+      end
+    end
+    stack.pop
   end
 
   def postfix_respression
     output = []
     op_stack = []
-    @expression.scan(/([.\d]+)|([\+\-\*()])/) do |number, op|
+    # This regex captures numbers in one group, and any math operators in the other
+    @expression.scan(/([.\d]+)|([\/\+\-\*()])/) do |number, op|
+
       output << number.to_f if number
       if op
         op_stack.push op
       end
     end
+
+    output += op_stack.reverse
+
+    output
   end
    
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,43 +3,43 @@ require 'spec_helper'
 require 'calculator'
 
 describe Calculator do
-  it "runs a test" do
-    expect(true). to eq false
-  end
 
   context "Addition" do
-    skip
-    it "adds two numbers" dos
-    pass
-      expect(Calculator.result("1+1")).to eql 2
-    end
-    it "adds many numbers" do
-      pass
-      expect(Calculator.result("1+2+3+4+1+2+3+4")).to eql 20
+    it "adds two numbers" do
+      expect(Calculator.result("1+1")).to eq 2
     end
   end
 
   context "Subtraction" do
     it "subtracts two numbers" do
-      expect(Calculator.result("1+1")).to eql 2
-    end
-    it "adds and subtracts numbers" do
-      expect(Calculator.result("1+2-3+4")).to eql 20
+      expect(Calculator.result("5-1")).to eq 4
     end
   end
 
   context "Multiplication" do
     it "multiplies two numbers" do
-      expect(Calculator.result("5*5")).to eql 25
+      expect(Calculator.result("5*5")).to eq 25
     end
   end
 
   context "Division" do
     it "divides two numbers evenly" do
-      expect(Calculator.result("10/5")).to eql 2.0
+      expect(Calculator.result("10/5")).to eq 2
     end
     it "divides two numbers with a decimal result" do
-      expect(Calculator.result("15/5")).to eql 2.5
+      expect(Calculator.result("12.5/5")).to eq 2.5
     end
+  end
+
+  context "Multiple Operations" do
+    it "adds many numbers" do
+      expect(Calculator.result("1+2+3+4+1+2+3+4")).to eq 20
+    end
+
+    it "adds and subtracts numbers" do
+      # skip
+      expect(Calculator.result("1+2-3+4")).to eq 4
+    end
+
   end
 end
